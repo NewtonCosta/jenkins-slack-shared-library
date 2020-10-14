d0536f29488cce98d8c4a0ebed3c92907469ddf8
Newton Costa <newtongomez04@gmail.com>
2020-10-14 18:26:14 +0100

    changed successfull message

--- vars/sendNotificationSuccess.groovy
+++ vars/sendNotificationSuccess.groovy
@@ -1,7 +1,7 @@
 // Send Slack build successful notification
 def call() {
   sendNotifications(
-    slackSendColor: "#00FF00",
-    message: "SUCCESSFUL"
+    slackSendColor: "good" //"#00FF00",
+    message: "Hey good job. Code deployed SUCCESSFUL"
   )
 }
